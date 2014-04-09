60db6bb4135ad32c9004bb1dd0a2648990ee3aab
Samir Undavia <samir1@brandeis.edu>
2014-04-09 19:55:21 -0400

    added spotify links

--- spotify_seach_server.rb
+++ spotify_seach_server.rb
@@ -20,12 +20,20 @@ end
 get '/' do
 	content_type :json
 	hash = {}
-	MetaSpotify::Track.search(params[:q])[:tracks].each do |track|
+	num = 0
+	MetaSpotify::Track.search(params[:q])[:tracks].each_with_index do |track, index|
 		name = track.name
-		# album = track.album
-		artists = []
-		track.artists.each { |artist| artists.push artist.name }
-		hash[name]  = artists
+		artists = ""
+		track.artists.each_with_index do |artist, artist_index|
+			if artist_index == 0
+				artists += artist.name
+			else
+				artists += ", " + artist.name
+			end
+		end
+		uri = track.uri
+		link = "http://open." + uri.split(":")[0] + ".com/" + uri.split(":")[1] + "/" + uri.split(":")[2]
+		hash[index] = "<a href=\"" + link + "\" target=\"_blank\">" + name + " by " + artists + "</a><br />"
 	end
 	hash.to_json
 end
